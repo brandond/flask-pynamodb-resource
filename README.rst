8f4113dbd948301e6f1409eee9b9e6923fee7fae
Brad Davidson <brad@oatmail.org>
2018-08-24 00:17:53 +0000

    Update packaging metadata

---
+++ README.rst
@@ -0,0 +1,6 @@
+Flask PynamoDB Resource
+=======================
+
+Presents PynamoDB models (DynamoDB tables) as Flask-RESTful resources
+
+Consult `README.md on GitHub <https://github.com/brandond/flask-pynamodb-resource/blob/master/README.md>`__ for usage instructions.
--- setup.py
+++ setup.py
@@ -8,35 +8,36 @@ from setuptools import find_packages, setup
 
 chdir(dirname(abspath(__file__)))
 
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
-    name='flask-pynamodb-resource',
-    version_command=('git describe --tags --dirty', 'pep440-git-full'),
-    description='Presents PynamoDB models (DynamoDB tables) as Flask-RESTful resources',
     author='Brandon Davidson',
     author_email='brad@oatmail.org',
-    url='https://github.com/brandond/flask-pynamodb-resource',
-    packages=find_packages(),
-    include_package_data=False,
-    install_requires=[
-        'flask-restful',
-        'pynamodb',
-        'ujson',
-        'six',
-    ],
-    extras_require={
-        'dev': [
-            'setuptools-version-command',
-        ]
-    },
-    license='Apache',
-    python_requires='>=2.7',
     classifiers=[
         'Development Status :: 4 - Beta',
+        'Framework :: Flask',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Topic :: Database :: Front-Ends',
-        'Framework :: Flask'
     ],
+    description='Presents PynamoDB models (DynamoDB tables) as Flask-RESTful resources',
+    extras_require={
+        'dev': [
+            'setuptools-version-command',
+        ]
+    },
+    include_package_data=True,
+    install_requires=requirements,
+    long_description=readme,
+    name='flask-pynamodb-resource',
+    packages=find_packages(exclude=('docs')),
+    python_requires='>=2.7',
+    url='https://github.com/brandond/flask-pynamodb-resource',
+    version_command=('git describe --tags --dirty', 'pep440-git-full'),
 )
